78d785b0bd588e0761d06c8f3187d811b60740ba
Jason Means <means41@live.marshall.edu>
2017-11-03 22:16:37 -0400

    Large update with search function for child codes

--- Chirst Temple Kid Finder/Controllers/HomeController.cs
+++ Chirst Temple Kid Finder/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Chirst_Temple_Kid_Finder.Models;
 
 namespace Chirst_Temple_Kid_Finder.Controllers
 {
---
+++ Chirst Temple Kid Finder/Controllers/ParentController.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Chirst_Temple_Kid_Finder.Models;
+
+namespace Chirst_Temple_Kid_Finder.Controllers
+{
+    public class ParentController : Controller
+    {  
+        CodeDbContext db = new CodeDbContext();
+        public ActionResult Index(string search)
+        {
+            return View(db.CodeTables.Where(x => x.ChildCode.StartsWith(search) || search == null).ToList());
+        }
+
+        // GET: Parent/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Parent/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Parent/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Parent/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Parent/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Parent/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Parent/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
